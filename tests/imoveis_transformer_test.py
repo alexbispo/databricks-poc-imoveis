75a1641c6367c5738d8a6e23fa38ed67b670b5b5
alexbispo.artes@gmail.com <alexbispo.artes@gmail.com>
2023-11-22 19:36:37 +0000

    Primeiro unit test passando

---
+++ tests/imoveis_transformer_test.py
@@ -0,0 +1,61 @@
+from src.etl.bronze.imoveis import transformation as tf
+from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame
+
+jsonString = '''
+[
+  {
+    "ident": {
+      "customerID": "800002-TESTAAAC",
+      "source": "Website"
+    },
+    "listing": {
+      "types": {
+        "unit": "Outros",
+        "usage": "Residencial"
+      },
+      "address": {
+        "city": "Rio de Janeiro",
+        "location": {
+          "lon": -43.413557,
+          "lat": -22.909429
+        },
+        "zone": "Zona Oeste",
+        "neighborhood": "Taquara"
+      },
+      "prices": {
+        "price": "45000",
+        "tax": {
+          "iptu": "0",
+          "condo": "150"
+        }
+      },
+      "features": {
+        "bedrooms": 0,
+        "bathrooms": 0,
+        "suites": 0,
+        "parkingSpaces": 1,
+        "usableAreas": "62",
+        "totalAreas": "62",
+        "floors": 0,
+        "unitsOnTheFloor": 0,
+        "unitFloor": 0
+      }
+    }
+  }
+]
+'''
+
+def test_select_columns():
+    spark = SparkSession.builder \
+        .master("local[*]") \
+        .appName("POC Unit Tests") \
+        .getOrCreate()
+    
+
+    df = spark.read.json(spark.sparkContext.parallelize([jsonString]))
+
+    df = tf.select_columns(df)
+    
+    assert type(df) == DataFrame
+    #spark.stop()
